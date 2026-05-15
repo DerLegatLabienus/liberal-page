3784b0c0e16f577143c9a8a23fce3acabe17046f
Aviv Avitan <avivavitan63@gmail.com>
2026-05-16 01:48:31 +0300

    feat: wire all sections into App.tsx (full-width, no sidebar)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import BillsTracker from './components/BillsTracker';
+import Representatives from './components/Representatives';
+import UpdatesFeed from './components/UpdatesFeed';
+import PrimariesSection from './components/PrimariesSection';
+import ProtocolsList from './components/ProtocolsList';
+import JoinSection from './components/JoinSection';
+import Footer from './components/Footer';
 import './styles/globals.css';
 
 function App() {
   return (
     <>
       <Header />
-      <Hero />
+      <main>
+        <Hero />
+        <BillsTracker />
+        <Representatives />
+        <UpdatesFeed />
+        <PrimariesSection />
+        <ProtocolsList />
+        <JoinSection />
+      </main>
+      <Footer />
     </>
   );
 }
