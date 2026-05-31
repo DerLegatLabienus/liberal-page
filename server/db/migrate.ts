3c58d6ab872dfa2d3f3057381e41b79f0e341f6d
Aviv Avitan <avivavitan63@gmail.com>
2026-05-31 09:02:10 +0300

    Revert "debug"
    This reverts commit bc2d305b0ca7ec8039548a0bf6ad0e9647d0c370.

--- server/db/migrate.ts
+++ server/db/migrate.ts
@@ -11,6 +11,5 @@ export async function runMigrations(): Promise<void> {
     return
   }
   const { migrate } = await import('drizzle-orm/node-postgres/migrator')
-  console.log(process.env.DATABASE_URL)
   await migrate(db, { migrationsFolder: MIGRATIONS_DIR })
 }
